c638238e8288ed819d5335e45530a2af78e76004
YuriyBaev <43686783+YuriyBaev@users.noreply.github.com>
2020-02-13 12:31:24 +0400

    removed com.fasterxml.jackson.core:jackson-databind

--- student-service/src/main/java/student/model/dto/LessonDto.java
+++ student-service/src/main/java/student/model/dto/LessonDto.java
@@ -1,7 +1,6 @@
 package student.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,11 +15,10 @@ import java.util.List;
 @AllArgsConstructor
 public class LessonDto
 {
-    @JsonProperty("id")
     private String id;
-    @JsonProperty("studentNumber")
+
     private long studentNumber;
-    @JsonProperty("courseList")
+
     private List<String> courseList;
 
 }
