508da96ecb8b40d7ce65fc9ad93c2144c5ef9efd
YuriyBaev <43686783+YuriyBaev@users.noreply.github.com>
2020-02-13 16:34:39 +0400

    remove unused annotation

--- student-service/src/main/java/student/controller/StudentController.java
+++ student-service/src/main/java/student/controller/StudentController.java
@@ -1,7 +1,6 @@
 package student.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/students")
-@RefreshScope
 public class StudentController
 {
     private final StudentService studentService;
